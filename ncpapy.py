93732b9302c0be5fd86c121932ba24ccab7fd2f3
Ayron Catteau <ayronc@gmail.com>
2013-10-09 17:48:54 -0400

    Example of pad usage

--- ncpapy.py
+++ ncpapy.py
@@ -2,10 +2,6 @@
 
 import curses
 
-def draw_menu(w):
-
-  w.addstr(1, 1, "1: Playback  2: Recording  3: Config")
-
 def main(stdscr):
 
   # Keep background transperant
@@ -17,15 +13,21 @@ def main(stdscr):
   # Clear screen
   stdscr.clear()
 
-  stdscr.border()
+  # Draw menu
+  menuBorder = curses.newwin(3, curses.COLS, 0, 0)
+  menuBorder.border()
+  menuBorder.noutrefresh()
+  menu = curses.newpad(1, 50)
+  menu.addstr(0, 0, "  1: Playback  2: Recording  3: Config")
+  menu.noutrefresh( 0,0, 1,1, 1,curses.COLS-2)
 
-  draw_menu(stdscr)
+    
 
-  stdscr.refresh()
-  stdscr.getkey()
+  curses.doupdate()
+  menu.getkey()
 
 if __name__ == '__main__':
- 
+  
   curses.wrapper(main)
 
 
